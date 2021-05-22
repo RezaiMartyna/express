b512ccb9df2dd562fe17d0eed2510cb13e707fed
RezaiMartyna <73740511+RezaiMartyna@users.noreply.github.com>
2021-05-22 14:06:14 +0200

    contact

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+
 const app = express();
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
 
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
 
 
 app.use('/user/', (req, res,) => {
@@ -19,6 +24,26 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');  
+});
+
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const picture = req.file;
+
+
+  if(author && sender && title && message && picture) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
 });
 
 app.get('/hello/:name', (req, res) => {
